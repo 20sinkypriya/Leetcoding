bf7948cea52943818d52d36938c3ada79fad0a0a
Nilakshi13 <nilakshithool03@gmail.com>
2024-10-28 12:57:46 +0530

    Added GreddyAlgoProblems

--- GreedyAlgoProblems/BuyTwoChocolates.py
+++ GreedyAlgoProblems/BuyTwoChocolates.py
@@ -1,4 +1,5 @@
-# Here is the solution in Python.
+"""
+Difficulty Level - Easy
 You are given an integer array prices representing the prices of various chocolates in a store. You are also given a single integer money, which represents your initial amount of money.
 You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
 Return the amount of money you will have leftover after buying the two chocolates. If there is no way for you to buy two chocolates without ending up in debt, return money. Note that the leftover must be non-negative.
@@ -17,8 +18,7 @@ Constraints:
 2 <= prices.length <= 50
 1 <= prices[i] <= 100
 1 <= money <= 100
-
-SOLUTION:
+"""
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         ans=money-sum(sorted(prices)[:2])
